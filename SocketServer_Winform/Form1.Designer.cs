19ebfdeadddba7e68a85c730c719e75debfecb63
重铬酸钠 <sthenight@outlook.com>
2025-03-05 11:55:54 +0800

    使用 Winform 制作服务端 UI

---
+++ SocketServer_Winform/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace SocketServer_Winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.materialContextMenuStrip1 = new MaterialSkin.Controls.MaterialContextMenuStrip();
+            this.materialSingleLineTextField1 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialFlatButton2 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialFlatButton3 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.SuspendLayout();
+            // 
+            // materialContextMenuStrip1
+            // 
+            this.materialContextMenuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.materialContextMenuStrip1.Depth = 0;
+            this.materialContextMenuStrip1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialContextMenuStrip1.Name = "materialContextMenuStrip1";
+            this.materialContextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // materialSingleLineTextField1
+            // 
+            this.materialSingleLineTextField1.Depth = 0;
+            this.materialSingleLineTextField1.Hint = "Please enter message";
+            this.materialSingleLineTextField1.Location = new System.Drawing.Point(12, 77);
+            this.materialSingleLineTextField1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSingleLineTextField1.Name = "materialSingleLineTextField1";
+            this.materialSingleLineTextField1.PasswordChar = '\0';
+            this.materialSingleLineTextField1.SelectedText = "";
+            this.materialSingleLineTextField1.SelectionLength = 0;
+            this.materialSingleLineTextField1.SelectionStart = 0;
+            this.materialSingleLineTextField1.Size = new System.Drawing.Size(317, 23);
+            this.materialSingleLineTextField1.TabIndex = 2;
+            this.materialSingleLineTextField1.UseSystemPasswordChar = false;
+            // 
+            // materialRaisedButton1
+            // 
+            this.materialRaisedButton1.Depth = 0;
+            this.materialRaisedButton1.Location = new System.Drawing.Point(335, 77);
+            this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton1.Name = "materialRaisedButton1";
+            this.materialRaisedButton1.Primary = true;
+            this.materialRaisedButton1.Size = new System.Drawing.Size(75, 23);
+            this.materialRaisedButton1.TabIndex = 3;
+            this.materialRaisedButton1.Text = "Send";
+            this.materialRaisedButton1.UseVisualStyleBackColor = true;
+            this.materialRaisedButton1.Click += new System.EventHandler(this.materialRaisedButton1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(12, 106);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(398, 289);
+            this.textBox1.TabIndex = 5;
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.AutoSize = true;
+            this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.Enabled = false;
+            this.materialFlatButton1.Location = new System.Drawing.Point(417, 125);
+            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = false;
+            this.materialFlatButton1.Size = new System.Drawing.Size(96, 36);
+            this.materialFlatButton1.TabIndex = 6;
+            this.materialFlatButton1.Text = "Server info";
+            this.materialFlatButton1.UseVisualStyleBackColor = true;
+            this.materialFlatButton1.Click += new System.EventHandler(this.materialFlatButton1_Click);
+            // 
+            // materialFlatButton2
+            // 
+            this.materialFlatButton2.AutoSize = true;
+            this.materialFlatButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton2.Depth = 0;
+            this.materialFlatButton2.Location = new System.Drawing.Point(417, 77);
+            this.materialFlatButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton2.Name = "materialFlatButton2";
+            this.materialFlatButton2.Primary = false;
+            this.materialFlatButton2.Size = new System.Drawing.Size(55, 36);
+            this.materialFlatButton2.TabIndex = 7;
+            this.materialFlatButton2.Text = "Start";
+            this.materialFlatButton2.UseVisualStyleBackColor = true;
+            this.materialFlatButton2.Click += new System.EventHandler(this.materialFlatButton2_Click);
+            // 
+            // materialFlatButton3
+            // 
+            this.materialFlatButton3.AutoSize = true;
+            this.materialFlatButton3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton3.Depth = 0;
+            this.materialFlatButton3.Enabled = false;
+            this.materialFlatButton3.Location = new System.Drawing.Point(417, 173);
+            this.materialFlatButton3.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton3.Name = "materialFlatButton3";
+            this.materialFlatButton3.Primary = false;
+            this.materialFlatButton3.Size = new System.Drawing.Size(80, 36);
+            this.materialFlatButton3.TabIndex = 8;
+            this.materialFlatButton3.Text = "User info";
+            this.materialFlatButton3.UseVisualStyleBackColor = true;
+            this.materialFlatButton3.Click += new System.EventHandler(this.materialFlatButton3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(529, 407);
+            this.Controls.Add(this.materialFlatButton3);
+            this.Controls.Add(this.materialFlatButton2);
+            this.Controls.Add(this.materialFlatButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.materialRaisedButton1);
+            this.Controls.Add(this.materialSingleLineTextField1);
+            this.Name = "Form1";
+            this.Text = "Socket Server";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MaterialSkin.Controls.MaterialContextMenuStrip materialContextMenuStrip1;
+        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField1;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton2;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton3;
+    }
+}
+
---
+++ SocketServer_Winform/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+using MaterialSkin;
+using System.Net.Sockets;
+using System.Net;
+using static System.Net.Mime.MediaTypeNames;
+using System.Reflection.Emit;
+
+namespace SocketServer_Winform
+{
+    public partial class Form1 : MaterialSkin.Controls.MaterialForm
+    {
+        private static Dictionary<Socket, string> clients = new Dictionary<Socket, string>();
+        private static Socket Server;
+        internal static TextBox msgTextBox;
+        public Form1()
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+
+            msgTextBox = textBox1;
+            msgTextBox.Text = "服务端未启动";
+        }
+        public static void StartServer()
+        {
+            Server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                Server.Bind(new IPEndPoint(IPAddress.Any, 12345));
+                Server.Listen(10);
+            }
+            catch (Exception ex)
+            {
+                msgTextBox.AppendText(ex.Message);
+            }
+            AppentMsgText_Fast($@"服务端已启动，等待客户端连接...");
+
+            Task.Run(() => AcceptClient());
+        }
+        private static async Task AcceptClient()
+        {
+            while (true)
+            {
+                Socket client = Server.Accept(); // 阻塞等待客户端连接
+                AppentMsgText_Fast($"监听到新的用户尝试连接：{client.RemoteEndPoint}");
+                Task.Run(() => ReceiveMsg(client)); // 异步执行接收消息的 Task
+            }
+        }
+        private static async Task ReceiveMsg(Socket client) // 从客户端接收消息的 Task
+        {
+            byte[] buffer = new byte[1024];
+            try
+            {
+                // 新用户第一条消息必定是注册昵称，因此单独处理
+                int userName_num = client.Receive(buffer);
+                clients.Add(client, Encoding.UTF8.GetString(buffer, 0, userName_num));
+                AppentMsgText_Fast(clients[client] + "已注册");
+                Task.Run(() => Boardcast($"{clients[client]}已注册", client)); // 广播消息，异步执行防止阻塞线程
+
+                while (true)
+                {
+                    // 其后的消息都是聊天消息
+                    int num = client.Receive(buffer);
+                    if (num == 0) // num 为 0 说明客户端已断开连接
+                        break;
+                    string message = Encoding.UTF8.GetString(buffer, 0, num);
+                    AppentMsgText_Fast($"{clients[client]}:{message}");
+                    Task.Run(() => Boardcast($"{clients[client]}:{message}", client)); // 广播消息，异步执行防止阻塞线程
+                }
+            }
+            catch (Exception ex)
+            {
+                // 客户端异常断开连接
+                AppentMsgText_Fast($"{clients[client]}:{ex.Message}");
+                AppentMsgText_Fast($"{clients[client]}已离开");
+                Boardcast($"{clients[client]}已离开", client);
+                clients.Remove(client);
+            }
+        }
+        private static async Task Boardcast(string message, Socket sender) // 广播消息的Task
+        {
+            try
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+                foreach (var client in clients.Keys)
+                {
+                    if (client != sender)
+                        client.Send(buffer);
+                }
+            }
+            catch (Exception ex)
+            {
+                AppentMsgText_Fast($"广播失败：{ex.Message}");
+            }
+        }
+        private async void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            string msg = materialSingleLineTextField1.Text;
+            AppentMsgText_Fast(msg);
+            materialSingleLineTextField1.Text = "";
+            await Task.Run(() => Boardcast($"Server:{msg}", null));
+        }
+        // 由于无法跨线程操作控件，因此使用委托代理控件操作
+        internal static void AppentMsgText(string msg)
+        {
+            if (msgTextBox.InvokeRequired)
+            {
+                msgTextBox.Invoke(new Action(() =>
+                {
+                    msgTextBox.AppendText(msg + "\r\n");
+                }));
+            }
+            else
+            {
+                msgTextBox.AppendText(msg + "\r\n");
+            }
+        }
+        // 追加文字的快速方法，不需要额外写 Task.Run
+        internal static void AppentMsgText_Fast(string msg)
+        {
+            Task.Run(() => AppentMsgText(msg));
+        }
+        // 锁定布局大小，实现方式较粗糙
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            this.ClientSize = new System.Drawing.Size(529, 407);
+        }
+
+        private void materialFlatButton2_Click(object sender, EventArgs e)
+        {
+            if (Server == null)
+            {
+                textBox1.Clear();
+                StartServer();
+                materialFlatButton1.Enabled = true;
+                materialFlatButton3.Enabled = true;
+                materialFlatButton2.Text = "Stop";
+            }
+            else
+            {
+                // 关闭服务端，由于停止 Task 较为麻烦，因此直接重启程序
+                System.Diagnostics.Process.Start(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                Environment.Exit(0);
+            }
+        }
+        private void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+            if (Server != null)
+            {
+                MessageBox.Show($@"Local EndPoint: {Server.LocalEndPoint}
+AddressFamily: {Server.AddressFamily}
+SocketType: {Server.SocketType}
+ProtocolType: {Server.ProtocolType}");
+            }
+        }
+        private void materialFlatButton3_Click(object sender, EventArgs e)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var item in clients.Keys)
+            {
+                stringBuilder.Append($"Username: {clients[item]}\nEndPoint: {item.LocalEndPoint}\r\n\r\n");
+            }
+            // 可爱的三元表达式
+            MessageBox.Show(stringBuilder.ToString() == "" ? "未找到用户" : $"{stringBuilder.ToString()}");
+        }
+    }
+}
